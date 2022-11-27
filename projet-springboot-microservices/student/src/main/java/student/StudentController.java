6aab1017080d383a4f1f4f5ea9744451fc25445c
maroung13 <marounghossain1@gmail.com>
2022-11-27 23:59:17 +0200

    fixes

--- projet-springboot-microservices/student/src/main/java/student/StudentController.java
+++ projet-springboot-microservices/student/src/main/java/student/StudentController.java
@@ -2,6 +2,7 @@ package student;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
