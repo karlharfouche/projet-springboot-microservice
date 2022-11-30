ccf7f05955c36b16cd2210cfb9cf337465a9c480
maroung13 <marounghossain1@gmail.com>
2022-11-30 23:15:01 +0200

    Add eureka server

--- projet-springboot-microservices/evaluation/src/main/java/evaluation/EvaluationApplication.java
+++ projet-springboot-microservices/evaluation/src/main/java/evaluation/EvaluationApplication.java
@@ -2,8 +2,10 @@ package evaluation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EvaluationApplication {
     public static void main(String[] args) {
         SpringApplication.run(EvaluationApplication.class, args);
